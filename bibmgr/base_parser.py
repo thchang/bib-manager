45ce59686575e9a01129d7f9eee7d4595daea65d
thchang <thchang@vt.edu>
2024-09-30 17:19:42 -0400

    updated parser structure

--- bibmgr/base_parser.py
+++ bibmgr/base_parser.py
@@ -1,8 +1,7 @@
 class Parser:
 
     def __init__(self):
-
-        self.info = {
+        self.template = {
             'authors': [],
             'title': None,
             'year': None,
@@ -17,52 +16,57 @@ class Parser:
             'descrip': None,
             'tags': [],
         }
+        self.nextItem = None
+        self.nextKey = None
+        self.info = []
 
     def parse_authors(self, authors):
-        self.info['author'] = authors
+        self.nextItem['author'] = authors
 
     def parse_title(self, title):
-        self.info['title'] = title
+        self.nextItem['title'] = title
 
     def parse_year(self, year):
-        self.info['year'] = year
+        self.nextItem['year'] = year
 
     def parse_publisher(self, publisher):
-        self.info['publisher'] = publisher
+        self.nextItem['publisher'] = publisher
 
     def parse_venue(self, venue):
-        self.info['venue'] = venue
+        self.nextItem['venue'] = venue
 
     def parse_doi(self, doi):
-        self.info['doi'] = doi
+        self.nextItem['doi'] = doi
 
     def parse_url(self, url):
-        self.info['url'] = url
+        self.nextItem['url'] = url
 
     def parse_isbn(self, isbn):
-        self.info['isbn'] = isbn
+        self.nextItem['isbn'] = isbn
 
     def parse_git(self, git):
-        self.info['git'] = git
+        self.nextItem['git'] = git
 
     def parse_web(self, web):
-        self.info['web'] = web
+        self.nextItem['web'] = web
 
     def parse_note(self, note):
-        self.info['note'] = note
+        self.nextItem['note'] = note
 
     def parse_descrip(self, descrip):
-        self.info['descrip'] = descrip
+        self.nextItem['descrip'] = descrip
 
     def add_tag(self, tag):
-        self.info['tags'].append(tag)
+        self.nextItem['tags'].append(tag)
 
     def read(self, filename):
         pass
 
     def write_yaml(self, filename):
-        with open(filename, "a") as fp:
-            fp.write()
+        with open(filename, "w") as fp:
+            yaml.dump(self.info, fp)
 
     def write_bibtex(self, filename):
-        pass
+        with open(filename, "w") as fp:
+            for key in self.info:
+                self.info[key]

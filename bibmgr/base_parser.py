4afbf094e39523999198f1ad45ed2d3c5d058014
thchang <thchang@vt.edu>
2024-10-01 10:39:58 -0400

    modified parser to handle all

--- bibmgr/base_parser.py
+++ bibmgr/base_parser.py
@@ -20,47 +20,48 @@ class Parser:
         self.nextKey = None
         self.info = []
 
-    def parse_authors(self, authors):
+    def parse_bib_authors(self, authors):
         self.nextItem['author'] = authors
 
-    def parse_title(self, title):
+    def parse_bib_title(self, title):
         self.nextItem['title'] = title
 
-    def parse_year(self, year):
+    def parse_bib_year(self, year):
         self.nextItem['year'] = year
 
-    def parse_publisher(self, publisher):
+    def parse_bib_publisher(self, publisher):
         self.nextItem['publisher'] = publisher
 
-    def parse_venue(self, venue):
+    def parse_bib_venue(self, venue):
         self.nextItem['venue'] = venue
 
-    def parse_doi(self, doi):
+    def parse_bib_doi(self, doi):
         self.nextItem['doi'] = doi
 
-    def parse_url(self, url):
+    def parse_bib_url(self, url):
         self.nextItem['url'] = url
 
-    def parse_isbn(self, isbn):
+    def parse_bib_isbn(self, isbn):
         self.nextItem['isbn'] = isbn
 
-    def parse_git(self, git):
+    def parse_bib_git(self, git):
         self.nextItem['git'] = git
 
-    def parse_web(self, web):
+    def parse_bib_web(self, web):
         self.nextItem['web'] = web
 
-    def parse_note(self, note):
+    def parse_bib_note(self, note):
         self.nextItem['note'] = note
 
-    def parse_descrip(self, descrip):
+    def parse_bib_descrip(self, descrip):
         self.nextItem['descrip'] = descrip
 
     def add_tag(self, tag):
         self.nextItem['tags'].append(tag)
 
-    def read(self, filename):
-        pass
+    def read_yaml(self, filename):
+        with open(filename, "r") as fp:
+            self.info = yaml.safe_load(fp)
 
     def write_yaml(self, filename):
         with open(filename, "w") as fp:

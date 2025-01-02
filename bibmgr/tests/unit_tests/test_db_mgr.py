c35c7cc6253ab810349d5c4f9b3cef82ffc51b25
thchang <thchang@vt.edu>
2025-01-02 10:22:25 -0800

    configured flake8 and unit tests

--- bibmgr/tests/unit_tests/test_db_mgr.py
+++ bibmgr/tests/unit_tests/test_db_mgr.py
@@ -2,7 +2,7 @@ import os
 import filecmp
 
 from bibmgr.db_mgr import DbMgr
-from bibmgr.test.unit_test.soln_data import soln
+from bibmgr.tests.unit_tests.soln_data import soln
 
 class TestDbMgr:
 
@@ -15,15 +15,15 @@ class TestDbMgr:
                 assert soln[item][key] == self.tester.info[item][key]
 
     def test_read_bibtex(self):
-        self.tester.read_bibtex("bibmgr/test/unit_test/test.bib")
+        self.tester.read_bibtex("bibmgr/tests/unit_tests/test.bib")
         self.check_results()
 
     def test_read_yaml(self):
-        self.tester.read_yaml("bibmgr/test/unit_test/test.yaml")
+        self.tester.read_yaml("bibmgr/tests/unit_tests/test.yaml")
         self.check_results()
 
     def test_write_yaml(self):
         self.tester.write_yaml("test2.yaml")
         assert os.path.exists("test2.yaml")
-        assert filecmp.cmp("bibmgr/test/unit_test/test.yaml", "test2.yaml")
+        assert filecmp.cmp("bibmgr/tests/unit_tests/test.yaml", "test2.yaml")
         os.remove("test2.yaml")
